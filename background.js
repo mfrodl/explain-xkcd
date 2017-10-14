46a290dfa34e8e4179104aeaab2721baf39f3eb8
Martin Frodl <maarilainen@gmail.com>
2017-10-14 22:48:21 +0200

    Set popup whenever tab is activated or updated
    Fixes #1

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
-chrome.webNavigation.onCompleted.addListener(function(details) {
-  var url = new URL(details.url);
-  if (url.hostname != 'xkcd.com') {
-    chrome.browserAction.setPopup({popup: '', tabId: details.tabId});
+function setPopup(tabId, url) {
+  var url = new URL(url);
+  var popup = (url.hostname == 'xkcd.com' ? 'popup.html' : '');
+  chrome.browserAction.setPopup({popup: popup, tabId: tabId});
+}
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    setPopup(activeInfo.tabId, tabs[0].url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.url !== undefined) {
+    setPopup(tabId, changeInfo.url);
   }
 });
